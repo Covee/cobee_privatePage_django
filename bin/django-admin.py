991643d5a64dca226854d2967d3046ac6fce006f
Covee <uniqtop91@gmail.com>
2018-08-19 16:32:08 +0900

    간단한 my page 만들꺼얌

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cobee/Desktop/django_files/my_profile_page/v_profile/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
