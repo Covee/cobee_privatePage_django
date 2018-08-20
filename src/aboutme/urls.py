b1874c2a234369d8a0d14438d285bed55fdb8a4d
Covee <uniqtop91@gmail.com>
2018-08-20 15:44:09 +0900

    little update

--- src/aboutme/urls.py
+++ src/aboutme/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, About, Skills, Contact
 
 
 urlpatterns = [
-	path('', MainView.as_view(), name='home')
+	path('', MainView.as_view(), name='home'),
+	path('about/', About.as_view(), name='about'),
+	path('skills/', Skills.as_view(), name='skills'),
+	path('contact/', Contact.as_view(), name='contact'),
+	# path('', MainView.as_view(), name='home'),
+
 ]
--- src/aboutme/views.py
+++ src/aboutme/views.py
@@ -4,3 +4,22 @@ from django.views.generic import TemplateView
 
 class MainView(TemplateView):
 	template_name = 'home.html'
+
+
+class About(TemplateView):
+	template_name = 'about.html'
+
+class Skills(TemplateView):
+	template_name = 'skills.html'
+
+# class Experience(TemplateView):
+# 	template_name = 'experience.html'
+
+class Contact(TemplateView):
+	template_name = 'contact.html'
+
+# class About(TemplateView):
+# 	template_name = 'about.html'
+
+# class About(TemplateView):
+# 	template_name = 'about.html'
